2c181fde987a435056636447bcc57a7d4578ba73
SerpentBTW <serpbtw@gmail.com>
2023-05-27 20:36:27 -0400

    Initialize

---
+++ main.py
@@ -0,0 +1,13 @@
+import json
+
+
+with open("100percentProject\main.json") as f:
+  data = json.load(f)
+
+
+
+
+
+#gameCount = len(data["Games"])
+#rand = random.randint(0, gameCount - 1 )
+#print(data["Games"][rand])
